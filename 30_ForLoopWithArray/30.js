69cb4de9c3edbd119b3c5d3c4e80862449976406
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2022-06-19 16:45:32 +0530

    while loop

--- 30_ForLoopWithArray/30.js
+++ 30_ForLoopWithArray/30.js
@@ -1,11 +1,13 @@
 let fruits = ["apple","mango","grapes","banana"];
 
-console.log(fruits.length);
-console.log(fruits[fruits.length-1]);
-
-
+// console.log(fruits.length);
+// console.log(fruits[fruits.length-1]);
 for (let i = 0; i < fruits.length; i++) {
-    const element = fruits[i];
-    console.log(element)
+    console.log(fruits[i]);
 }
+let fruits2 = []
+for (let i = 0; i < fruits.length; i++) {
+    fruits2.push(fruits[i].toUpperCase());
+}
+console.log(fruits2);
 
---
+++ 31UseConstForArray/31.js
@@ -0,0 +1,8 @@
+// const pi = 3.14;
+// pi = 12;
+// console.log(pi);
+
+
+const fruits = ["apple","mango","grapes"];
+fruits.push("banana");
+console.log(fruits);
