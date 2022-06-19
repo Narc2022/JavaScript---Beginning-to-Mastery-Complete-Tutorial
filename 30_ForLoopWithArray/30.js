6c1bf6c28d2ebdbb87507e2a0281163fe2497392
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2022-06-19 07:28:21 +0530

    push

---
+++ 30_ForLoopWithArray/30.js
@@ -0,0 +1,11 @@
+let fruits = ["apple","mango","grapes","banana"];
+
+console.log(fruits.length);
+console.log(fruits[fruits.length-1]);
+
+
+for (let i = 0; i < fruits.length; i++) {
+    const element = fruits[i];
+    console.log(element)
+}
+
