2c71a30136902c9c0b2a25dfd7d9297e058f647e
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2022-07-04 21:24:30 +0530

    IterateObject

--- 38IterateObject/38.js
+++ 38IterateObject/38.js
@@ -1,11 +1,9 @@
-const key = 'email';
 const person = {
-    name:"Sachin",
-    age:26,
-    hobbies:["machine","music","programming"]
+    name:"sachin",
+    age:'26',
+    "Hobbies":["Business","Inovataion","Development"]
 }
 
 for(let key in person){
-    // console.log(`${key} : ${person[key]}`);
-    console.log(key,":", person[key]);
+    console.log("key",":",person[key]);
 }
