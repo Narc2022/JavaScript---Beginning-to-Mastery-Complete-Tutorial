b9a8049dd790c5b1e1ed8edcb6c3e2c7acd75ff9
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2022-07-06 21:06:08 +0530

    38.js

--- 38IterateObject/38.js
+++ 38IterateObject/38.js
@@ -1,9 +1,26 @@
+//ItreateObject
+
+
+
+
 const person = {
     name:"sachin",
     age:'26',
     "Hobbies":["Business","Inovataion","Development"]
 }
 
-for(let key in person){
-    console.log("key",":",person[key]);
-}
+//for in loop
+
+// for(let key in person){                     
+//     //console.log(person[key]);               //only key
+//     //console.log("key",":",person[key]);     //value with key
+//     //console.log(`${key} : ${person[key]}`); //with templete string
+//     //console.log(key,":",person[key]); 
+// }
+
+//Object.keys
+
+console.log(Object.keys(person));
+console.log(typeof (Object.keys(person)));
+const val = Array.isArray(Object.keys(person));
+console.log(val);
