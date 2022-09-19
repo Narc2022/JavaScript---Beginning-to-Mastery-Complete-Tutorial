7bff3020e65f0300714cc7bc352b8ac786f45243
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2022-09-19 21:09:22 +0530

    psh

--- 38IterateObject/38.js
+++ 38IterateObject/38.js
@@ -1,26 +1,34 @@
 //ItreateObject
-
-
-
-
 const person = {
     name:"sachin",
     age:'26',
     "Hobbies":["Business","Inovataion","Development"]
 }
 
-//for in loop
+/*for in loop
+Object.keys*/
 
-// for(let key in person){                     
-//     //console.log(person[key]);               //only key
-//     //console.log("key",":",person[key]);     //value with key
-//     //console.log(`${key} : ${person[key]}`); //with templete string
-//     //console.log(key,":",person[key]); 
+// for(let key in person){
+//     console.log(key," : ",person[key]);
 // }
 
-//Object.keys
+// // output 
+// // name  :  sachin
+// // age  :  26
+// // Hobbies  :  [ 'Business', 'Inovataion', 'Development' ]
 
-console.log(Object.keys(person));
-console.log(typeof (Object.keys(person)));
-const val = Array.isArray(Object.keys(person));
-console.log(val);
+// console.log(typeof (Object.keys(person)));        //output = object
+// const val =Array.isArray((Object.keys(person)));   
+// console.log(val);                                 //output = true
+
+/*for of loop*/
+
+// for(let key of Object.keys(person)){
+//      console.log(person[key]);
+// }
+
+/*output
+
+sachin
+26
+[ 'Business', 'Inovataion', 'Development' ]*/
---
+++ 39ComputedPeoperties/39.js
@@ -0,0 +1,27 @@
+const key1 = "objkey1";
+const key2 = "objkey2";
+
+const value1 = "myvalue1";
+const value2 = "myvalue2";
+
+//normal method 
+const obj = {};
+obj[key1] = value1;
+obj[key2] = value2;
+console.log(obj);
+
+// const obj = {
+//     objkey1 : "myvalue1",
+//     objkey2 : "myvalue2"
+// }
+// console.log(obj);
+
+
+
+// computer properties
+// const obj = {
+//     [key1] : value1,
+//     [key2] : value2
+// }
+// console.log(obj);
+
